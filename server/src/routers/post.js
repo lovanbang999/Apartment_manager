32d08e3bb75250bb3ccf4f5ad4f17233651c4e42
LoVanBang <lovanbangbox9@gmail.com>
2023-09-24 19:17:57 +0700

    Create and build base middleware/auth and route post

--- server/src/routers/post.js
+++ server/src/routers/post.js
@@ -2,12 +2,77 @@ const express = require('express');
 const router = express.Router();
 
 const Post = require('../app/models/Post');
+const verifyToken = require('../middleware/auth');
+const { verify } = require('argon2');
+
+// @router [GET] /api/posts
+// @desc Get post
+// @access Private
+router.get('/', verifyToken, async (req, res) => {
+    try {
+        const posts = await Post.find({ user: req.userId }).populate('user', ['username']);
+        res.json({ success: true, posts });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error!' });
+    }
+});
+
+// @router [PUT] /api/posts
+// @desc Update post
+// @access Private
+router.put('/:id', verifyToken, async (req, res) => {
+    const { title, description, url } = req.body;
+
+    // Simple vadidation
+    if (!title) return res.status(400).json({ success: false, message: 'Title is required' });
+
+    try {
+        let updatePost = {
+            title,
+            description: description || '',
+            url: (url.startsWith('https://') ? url : `https://${url}`) || '',
+        };
+
+        const postUpdateCondition = { _id: req.params.id, user: req.userId };
+
+        updatePost = await Post.findOneAndUpdate(postUpdateCondition, updatePost, { new: true });
+
+        // User not authorized to update post or post not found
+        if (!updatePost)
+            return res.status(401).json({ success: false, message: 'Post not found or user not authorized' });
+
+        res.status(200).json({ success: true, message: 'Updated post successfully', post: updatePost });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error!' });
+    }
+});
+
+// @router [DELETE] /api/posts
+// @desc Delete post
+// @access Private
+router.delete('/:id', verifyToken, async (req, res) => {
+    try {
+        const postDeleteCondition = { _id: req.params.id, user: req.userId };
+
+        const deletePost = await Post.findOneAndDelete(postDeleteCondition);
+
+        // User not authorized or post not found
+        if (!deletePost)
+            return res.status(401).json({ success: false, message: 'Post not found or user not authorized' });
+
+        res.status(200).json({ success: true, message: 'Deleted post successfully', post: deletePost });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error!' });
+    }
+});
 
 // @router [POST] /api/posts
 // @desc Create post
 // @access Private
-
-router.post('/', async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
     const { title, description, url } = req.body;
 
     // Simple vadidation
@@ -18,7 +83,7 @@ router.post('/', async (req, res) => {
             title,
             description,
             url: url.startsWith('https://') ? url : `https://${url}`,
-            user: '650f0b5fcefc7361b7590a4c',
+            user: req.userId,
         });
 
         await newPost.save();
