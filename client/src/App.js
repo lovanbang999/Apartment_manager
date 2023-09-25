34f45fc97292e35290ed870e32b2e6b156c319dc
LoVanBang <lovanbangbox9@gmail.com>
2023-09-25 19:13:35 +0700

    Client: Create a client directory and install some support packages

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+    return <div className="app">Hello world</div>;
+}
+
+export default App;
